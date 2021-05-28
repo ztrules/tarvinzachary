c4497c7d3c3e47a3ad8c294681219230143ca07f
ztrules <tarvinzachary@gmail.com>
2021-05-28 09:09:28 -0500

    fixed Javascript

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -388,6 +388,6 @@ if (mark.BMI > john.BMI) {
 // console.log("Lifting weights repetition 10");
 
 // for loop keeps running while condition is TRUE
-for (let rep = 1; rep <= 10; rep = rep++) {
+for (let rep = 1; rep <= 10; rep++) {
     console.log("Lifting weights repetition 1");
 }
