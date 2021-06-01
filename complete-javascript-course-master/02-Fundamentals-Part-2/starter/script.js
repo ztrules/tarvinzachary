d0a1832945a14213f7433d34fd258041d68dcd68
ztrules <tarvinzachary@gmail.com>
2021-06-01 10:01:20 -0500

    worked on Javascript, learned about loops

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -435,3 +435,48 @@ for (let i = 0; i < jonas.length; i++) {
     console.log(jonas[i], typeof jonas[i]);
 };
 */
+
+// Looping Backwards and Loops in Loops
+
+/*
+const jonas = [
+    "Jonas",
+    "Schmedtmann",
+    2037 - 1991,
+    "Teacher",
+    ["Michael", "Peter", "Steven"],
+    true
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep <= 5; rep++)
+        console.log(`Exercise ${exercise}: Lifting weights repetition ${rep}`);
+}
+*/
+
+// The While Loop
+
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// let rep = 1
+// // while (rep <= 10) {
+// //     console.log(`Lifting weights repetition ${rep}`);
+// rep++
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1
+    if (dice === 6) console.log("Loop is about to end...")
+}
