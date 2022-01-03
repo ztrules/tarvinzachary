979e86b96814f22fe37d3f0d3cd7d8b42d9a560a
tarvinzachary <tarvinzachary@gmail.com>
2022-01-03 11:22:06 -0600

    learned about objects vs primitives

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -77,7 +77,7 @@ const z = 3;
 console.log(x === window.x)
 console.log(y === window.y)
 console.log(z === window.z)
-*/
+
 
 // console.log(this);
 
@@ -111,3 +111,65 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
+*/
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
+
+//         // Solution 1
+//         // const self = this;
+//         // const isMillenial = function() {
+//         //     console.log(self);
+//         //     console.log(self.year >= 1981 && self.year <=
+//         //     1996);
+//         // };
+
+//         // Solution 2
+//         const isMillenial = () => {
+//             console.log(this);
+//             console.log(this.year >= 1981 && this.year <=
+//             1996);
+//         };
+//         isMillenial();
+//     },
+    
+//     greet: () => {
+//     console.log(this);
+//     console.log(`Hey ${this.firstName}`);
+//     }
+// };
+// jonas.greet();
+// jonas.calcAge()
+
+// const addExpr = function(a, b) {
+//     console.log(arguments);
+//     return a + b
+// };
+// addExpr(2, 5);
+// addExpr(2, 5, 8, 12);
+
+// var addArrow = (a, b) => {
+//     console.log(arguments);
+//     return a + b;
+// };
+// addArrow(2, 5, 8);
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me)
