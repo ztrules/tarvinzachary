2dba99215b55e7658e489428fa908150fe616156
tarvinzachary <tarvinzachary@gmail.com>
2022-01-25 11:49:28 -0600

    Finished Coding Challenge 3

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -58,6 +58,42 @@ const restaurant = {
   },
 };
 
+// Coding Challenge 3
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(
+  `On average, an event happened every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+/*
 // Maps: Iteration
 
 const question = new Map([
@@ -93,7 +129,6 @@ console.log(...question);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 
-/*
 // Maps: Fundamentals
 
 const rest = new Map();
