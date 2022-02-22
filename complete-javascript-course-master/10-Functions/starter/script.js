d9c4cb2cb86b30583d681fa0bd519a85f3390e83
tarvinzachary <tarvinzachary@gmail.com>
2022-02-22 12:03:52 -0600

    Started Section 10, learned about default values

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH374', 2);
+createBooking('LH771', 5);
+createBooking('LH902', undefined, 1000);
