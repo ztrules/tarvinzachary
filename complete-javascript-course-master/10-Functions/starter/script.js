31fec6d8485b0a69b75aec59e9993b1804041275
tarvinzachary <tarvinzachary@gmail.com>
2022-02-23 16:36:55 -0600

    Learned about values vs references

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+// Default Parameters
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +27,38 @@ createBooking('LH123', 2, 800);
 createBooking('LH374', 2);
 createBooking('LH771', 5);
 createBooking('LH902', undefined, 1000);
+*/
+
+// How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in!');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
